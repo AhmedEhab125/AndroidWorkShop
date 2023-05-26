ea1cfde0d8afdae2209076b25bce94fd0ff7503e
AhmedEhab125 <121432823+AhmedEhab125@users.noreply.github.com>
2023-05-26 21:35:44 +0300

    submit after review

--- app/src/main/java/com/example/myapplication/favorite/favoriteView/FavRecyclerView.kt
+++ app/src/main/java/com/example/myapplication/favorite/favoriteView/FavRecyclerView.kt
@@ -10,26 +10,31 @@ import com.example.myapplication.model.NewsModel
 
 class FavRecyclerView(var newsList: List<Articles>) :
     RecyclerView.Adapter<FavRecyclerView.ViewHolder>() {
-    lateinit var binding: FavIteamBinding
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         var layoutInflater = LayoutInflater.from(parent.context)
-        binding = FavIteamBinding.inflate(layoutInflater, parent, false)
+        var binding = FavIteamBinding.inflate(layoutInflater, parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvFavTitle.text= newsList[position].title
-        holder.binding.tvFavDiscribtion.text = newsList[position].description
-        Glide.with(binding.root).load(newsList[position].urlToImage).into(holder.binding.ivFavNews)
+        holder.setArticleData(newsList[position])
     }
 
     override fun getItemCount(): Int {
         return newsList.size
     }
-    fun setArticlsList( newsList: List<Articles>){
-        this.newsList=newsList
+
+    fun setArticlsList(newsList: List<Articles>) {
+        this.newsList = newsList
         notifyDataSetChanged()
     }
 
-    class ViewHolder(var binding: FavIteamBinding) : RecyclerView.ViewHolder(binding.root)
+    class ViewHolder(var binding: FavIteamBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun setArticleData(articles: Articles) {
+            binding.tvFavTitle.text = articles.title
+            binding.tvFavDiscribtion.text = articles.description
+            Glide.with(binding.root).load(articles.urlToImage).into(binding.ivFavNews)
+        }
+    }
 }
