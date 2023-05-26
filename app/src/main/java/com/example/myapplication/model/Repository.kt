59abc37b05964551f801438a5fa48b76817d0e8a
essam <essameldenmohamed@yahoo.com>
2023-05-26 16:13:43 +0200

    fix errors

--- app/src/main/java/com/example/myapplication/model/Repository.kt
+++ app/src/main/java/com/example/myapplication/model/Repository.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.flowOf
 
 class Repository(var remoteSourceInter: RemoteSourceInter) :RepositoryInterface {
 
-    override suspend fun registerUser(requestBody: SignUpModel): Flow<RetriveData?> {
+    override suspend fun registerUser(requestBody: SignUpModel) : RetriveData {
        return remoteSourceInter.registerUser(requestBody)
     }
     override suspend fun getUserNameAndPassword(): LoginUser {
--- app/src/main/java/com/example/myapplication/model/RepositoryInterface.kt
+++ app/src/main/java/com/example/myapplication/model/RepositoryInterface.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication.model
 
+import com.example.myapplication.login.loginView.LoginUser
 import com.example.myapplication.network.RemoteSourceInter
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryInterface :RemoteSourceInter{
-
-    suspend fun getUserNameAndPassword():LoginUser
+    override suspend fun registerUser(requestBody: SignUpModel) : RetriveData
+    suspend fun getUserNameAndPassword(): LoginUser
 }
