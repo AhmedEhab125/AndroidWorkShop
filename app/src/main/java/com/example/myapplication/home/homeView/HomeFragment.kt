89d13634591af282fd3a208a8920d973ae5fd239
essam <essameldenmohamed@yahoo.com>
2023-05-27 12:41:36 +0200

    editing loading and failure statse

--- app/src/main/java/com/example/myapplication/home/homeView/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/home/homeView/HomeFragment.kt
@@ -1,9 +1,12 @@
 package com.example.myapplication.home.homeView
 
+import android.app.ProgressDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,12 +26,14 @@ class HomeFragment : Fragment() {
     private lateinit var manager:LayoutManager
     private lateinit var homeFactory:HomeViewModelFactory
     private lateinit var homeViewModel:HomeViewModel
+    private lateinit var progressDialog: ProgressDialog
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-
+        progressDialog = ProgressDialog(context)
+        progressDialog.setMessage("loading")
         manager = LinearLayoutManager(context)
         homeFactory = HomeViewModelFactory(NewsRepo(NewsClinet()))
         homeViewModel = ViewModelProvider(this,homeFactory).get(HomeViewModel::class.java)
@@ -39,6 +44,12 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         homeViewModel.getNewsData()
+        homeViewModel.isLoading.observe(viewLifecycleOwner){
+            when(it){
+                true -> {progressDialog.show()}
+                else -> {progressDialog.hide()}
+            }
+        }
         homeViewModel.homeData.observe(viewLifecycleOwner){
             when(it){
                 is ApiState.Success<*> -> {
@@ -47,6 +58,11 @@ class HomeFragment : Fragment() {
                     adapter = FavRecyclerView(list)
                     binding.homeRV.layoutManager = manager
                     binding.homeRV.adapter = adapter
+                }
+                is ApiState.Failure -> {
+                    Toast.makeText(requireContext(),it.err.message, Toast.LENGTH_LONG).show()
+                    Log.i("Emessage", "error")
+                    //progressDialog.hide()
                 }
                 else -> {}
             }
