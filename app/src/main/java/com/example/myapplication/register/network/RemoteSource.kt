eb6bd64d7f470a1b3ebcd11220a68cc5c7e545b3
milad <miladsoliman55@gmail.com>
2023-05-27 16:05:06 +0200

    Adding Api Key In Gradel

--- app/src/main/java/com/example/myapplication/register/network/RemoteSource.kt
+++ app/src/main/java/com/example/myapplication/register/network/RemoteSource.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.register.network
 
+import com.example.myapplication.BuildConfig
 import com.example.myapplication.model.LoginUserModel
 import com.example.myapplication.model.RetriveData
 import com.example.myapplication.model.SignUpModel
@@ -10,11 +11,11 @@ import com.example.myapplication.network.RetrofitClass
 class RemoteSource : RemoteSourceInter {
     override suspend fun registerUser(requestBody: SignUpModel) : RetriveData {
 
-        return RetrofitClass.getInstance().registerUser(requestBody = requestBody)
+        return RetrofitClass.getInstance().registerUser(requestBody = requestBody , BuildConfig.APP_KEY_1)
     }
 
     override suspend fun loginUser(loignUserBody: LoginUserModel) : RetriveData {
-        return RetrofitClass.getInstance().loginUser(loignUserBody)
+        return RetrofitClass.getInstance().loginUser(loignUserBody , BuildConfig.APP_KEY_1)
     }
 
 
--- app/src/main/java/com/example/myapplication/utility/Constant.kt
+++ app/src/main/java/com/example/myapplication/utility/Constant.kt
@@ -4,6 +4,7 @@ class Constant {
 
     companion object {
        const val emailValidation = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$"
+
     }
 
 }
