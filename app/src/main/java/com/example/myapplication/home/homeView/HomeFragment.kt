5e680225bdae6ab8ea4a48eb37a5ca7edd50595e
milad <miladsoliman55@gmail.com>
2023-05-27 13:29:06 +0200

    handle network status

--- app/src/main/java/com/example/myapplication/home/homeView/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/home/homeView/HomeFragment.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication.home.homeView
 
+import android.app.AlertDialog
 import android.app.ProgressDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.myapplication.databinding.FragmentHomeBinding
@@ -19,6 +23,11 @@ import com.example.myapplication.home.model.NewsRepo
 import com.example.myapplication.home.newsOnlineDataSource.NewsClinet
 import com.example.myapplication.model.ApiState
 import com.example.myapplication.model.Articles
+import com.example.myapplication.network.NetworkConectivityObserver
+import com.example.myapplication.network.NetworkObservation
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -27,6 +36,8 @@ class HomeFragment : Fragment() {
     private lateinit var homeFactory:HomeViewModelFactory
     private lateinit var homeViewModel:HomeViewModel
     private lateinit var progressDialog: ProgressDialog
+    lateinit var networkObservation : NetworkObservation
+    var lastRequest = true
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,7 +53,24 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        networkObservation = NetworkConectivityObserver(requireContext())
+         lifecycleScope.launch {
+             networkObservation.observeOnNetwork().collectLatest {
+                 when(it.name){
+                      "Avaliavle" -> {
+                          if (!lastRequest){
+                              showInternetDialog()
+                          }
+                       Log.i("Internet",it.name)
 
+                     }
+                     "Lost" -> {
+                         Log.i("Internet",it.name)
+
+                     }
+                 }
+         }
+         }
         homeViewModel.getNewsData()
         homeViewModel.isLoading.observe(viewLifecycleOwner){
             when(it){
@@ -60,6 +88,7 @@ class HomeFragment : Fragment() {
                     binding.homeRV.adapter = adapter
                 }
                 is ApiState.Failure -> {
+                    lastRequest = false
                     Toast.makeText(requireContext(),it.err.message, Toast.LENGTH_LONG).show()
                      homeViewModel.getOfflineData()
                      homeViewModel.localData.observe(viewLifecycleOwner){
@@ -78,8 +107,32 @@ class HomeFragment : Fragment() {
 
     fun retry(){
         //calling setting data ()
-        // Ahmead's Logic here 
+        // Ahmead's Logic here
+        Log.i("Retray","Done")
+        homeViewModel.getNewsData()
+
     }
+
+
+    fun showInternetDialog() {
+        val retry = "Retray"
+        val builder = AlertDialog.Builder(context)
+        val message = "There Is No Internet Connection, Please Press Retry"
+        builder.setMessage(Html.fromHtml("<font color='#0000'>$message</font>"))
+        val title ="Check Conniction"
+        builder.setTitle(Html.fromHtml("<font color='#0000'>$title</font>"))
+        builder.setCancelable(false)
+        builder.setPositiveButton(Html.fromHtml("<font color='#0000'>$retry</font>"),
+            { dialog: DialogInterface?, which: Int ->
+               retry()
+            })
+
+
+        val alertDialog = builder.create()
+
+        alertDialog.show()
+    }
+
 
 
 }
